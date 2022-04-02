dcc453cbfa4d447a2f39468bd113d5f144404800
Jaydee Ballaho <jaydee.ballaho@wmsu.edu.ph>
2022-04-02 14:20:10 +0800

    Add gitignore

--- source/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
- $connection=mysqli_connect("localhost","root","","online_preadvising");
-
- if($connection === false){
- 	die("ERROR: COULD NOT CONNECT." .mysqli_connect_error());
- }
-?>
---
+++ source/includes/sample.config.php
@@ -0,0 +1,7 @@
+<?php 
+ $connection=mysqli_connect("localhost","u262410906_advising","","u262410906_advising");
+
+ if($connection === false){
+ 	die("ERROR: COULD NOT CONNECT." .mysqli_connect_error());
+ }
+?>
