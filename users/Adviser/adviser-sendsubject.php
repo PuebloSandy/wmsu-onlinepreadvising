680ae51365a3801b42e57c0403d795544eee618b
PuebloSandy <72772300+PuebloSandy@users.noreply.github.com>
2022-06-07 11:49:54 +0800

    Update adviser-sendsubject.php

--- users/Adviser/adviser-sendsubject.php
+++ users/Adviser/adviser-sendsubject.php
@@ -325,14 +325,14 @@
 			 $with = mysqli_fetch_array($get_preq);
 		         $with_preq = $with['subject_id'];
 		     }
-		     $select_prereq_sub_fk = mysqli_query($connection,"SELECT * FROM tblstudent_subject WHERE subject_id_fk='$with_preq' and student_id_fk='$Studid' and remarks in ('FAILED','Not Yet Taken') and curr_id_fk='$Currid' and course_id_fk='$courseid'");
+		     $select_prereq_sub_fk = mysqli_query($connection,"SELECT * FROM tblstudent_subject WHERE subject_id_fk='$with_preq' and student_id_fk='$Studid' and curr_id_fk='$Currid' and course_id_fk='$courseid'");
 		     if(mysqli_num_rows($select_prereq_sub_fk) > 0)
 		     {
 		      	 $withs = mysqli_fetch_array($select_prereq_sub_fk);
 		         $with_preqs = $with['id'];
 		     }
 		}
-		   if($with_preqs == $subjectID)
+		   if($subjectID == $with_preqs)
 		   {
 	    ?>
 			<div id="disable">
